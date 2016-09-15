484a09c1844808a37136b87c29619dbc54653eb9
Marc Annand <marc.annand@gmail.com>
2016-09-15 17:35:21 -0400

    Fixed geocode fetching for google map api by adding key in query. Was reaching daily quota without it.

--- game_calendar/helpers.py
+++ game_calendar/helpers.py
@@ -1,11 +1,12 @@
 import requests, urllib
 from django.utils.encoding import smart_str
+key = 'AIzaSyDlFVWD7CVtS_Sy6LeKkeKhaabRX6AfJV0'
 
 def get_lat(location):
 
     location = smart_str(location)
-    url = 'http://maps.googleapis.com/maps/api/geocode/json'
-    r = requests.get(url, params={'address': location})
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    r = requests.get(url, params={'address': location, 'key': key})
     data = r.json()
     if r.status_code == requests.codes.ok:
         lat = str(data['results'][0]['geometry']['location']['lat'])
@@ -16,8 +17,8 @@ def get_lat(location):
 def get_lng(location):
 
     location = smart_str(location)
-    url = 'http://maps.googleapis.com/maps/api/geocode/json'
-    r = requests.get(url, params={'address': location})
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    r = requests.get(url, params={'address': location, 'key': key})
     data = r.json()
     if r.status_code == requests.codes.ok:
         lng = str(data['results'][0]['geometry']['location']['lng'])
